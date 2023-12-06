45766939e957ee603dbe48221b6116c63d177d5b
demenSama <marcosvincius1051@gmail.com>
2023-12-05 22:29:26 -0300

    pojeto finalisado

--- projetando.js
+++ projetando.js
@@ -1,39 +1,44 @@
+console.log("antes de tudo quero explicar aos profissionais da Dio os quais estão lendo esse codigo que o mesmo apresenta algumas coisas a mais do que foi pedido no desafio, contudo acredito que ele ainda contem o que foi pedido. Obrigado pela atenção");
+console.log("\n");
+
 //criando projeto usando como base para tudo o if e else. 
 let heroi1 = "jack";
-let xpHero=0;
-let ranks =["ferro","Bronze","Prata","Ouro","Platina","Ascendente","Imortal","Radiante"]
+let xpHero=100;
+let ranks =["ferro","Bronze","Prata","Ouro","Platina","Ascendente","Imortal","Radiante"];
 let rank;
+
 //construindo o sistema de repetição para que o xp aumente
 while(xpHero<12000){
-    console.log(heroi1+ "apresenta " +xpHero+" logo ele está no rank "+ rank);
-    xpHero+=1000
-  
-
 //if e elses para os ranks
 if (xpHero<1000){
     rank=ranks[0]
 }else 
     if(xpHero>=1000 && xpHero<=2000){
-        rank=ranks[1]
+        rank=ranks[1];
     }else 
         if(xpHero>2000 && xpHero<=5000){
-            rank=ranks[2]
+            rank=ranks[2];
         }else 
             if(xpHero>5000 && xpHero<=7000){
-                rank=ranks[3]
+                rank=ranks[3];
             }else
                 if(xpHero>7000 && xpHero<=8000){
-                    rank=ranks[4]
+                    rank=ranks[4];
                 }else 
                     if(xpHero>8000 && xpHero<=9000){
-                        rank=ranks[5]
+                        rank=ranks[5];
                     }else
                         if(xpHero>9000 && xpHero<=10000){
-                            rank=ranks[6]
+                            rank=ranks[6];
                         }else 
                             if(xpHero>10000){
                                 rank=ranks[7];
                             }
 
+console.log("parabéns você ganhou mais "+ xpHero+ " de xp!");
+console.log("Agora o seu heroi de nome "+heroi1+ " apresenta " +xpHero+" de xp logo ele está no nivel "+ rank+"!");
+console.log("\n");
+//metodo de repetição usado
+ xpHero+=1000
 }
 
